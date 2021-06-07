3084f14aa409e7d2bc0e985de8e5e602d9a859e9
oereo <dlstpgns0406@gmail.com>
2021-06-07 23:34:20 +0900

    feat: add widget in screen_home

--- lib/screen/screen_home.dart
+++ lib/screen/screen_home.dart
@@ -25,6 +25,23 @@ class _HomeScreenState extends State<HomeScreen> {
               crossAxisAlignment: CrossAxisAlignment.center,
               children: <Widget>[
                 Center(child: Image.asset('images/무제.png', width: width * 0.8)),
+                Padding(
+                  padding: EdgeInsets.all(width * 0.024),
+                ),
+                Text(
+                  '플러터 퀴즈 앱',
+                  style: TextStyle(
+                    fontSize: width * 0.065,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                Text(
+                  '퀴즈를 풀기 전 안내사항입니다.\n꼼꼼히 읽고 퀴즈 풀기를 눌러주세요.',
+                  textAlign: TextAlign.center,
+                ),
+                Padding(
+                  padding: EdgeInsets.all(width * 0.048),
+                )
               ],
             )));
   }

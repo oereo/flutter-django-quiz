b0f82201c7ed798f951fc7d34ed99c2b62b96417
oereo <dlstpgns0406@gmail.com>
2021-06-08 13:23:17 +0900

    feat: add model_quiz.dart

---
+++ lib/model/model_quiz.dart
@@ -0,0 +1,11 @@
+class Quiz {
+  String title;
+  List<String> candidates;
+  int answer;
+
+  Quiz({this.title, this.candidates, this.answer});
+
+  Quiz.fromMap(Map<String, dynamic> map)
+  : title = map['title'],
+  candidates
+}

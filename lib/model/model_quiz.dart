f7d6977472b9a21cdea6b4d78e383f03456d2659
oereo <dlstpgns0406@gmail.com>
2021-06-08 16:40:06 +0900

    refactor: fix model, screen_home

--- lib/model/model_quiz.dart
+++ lib/model/model_quiz.dart
@@ -3,9 +3,13 @@ class Quiz {
   List<String> candidates;
   int answer;
 
-  Quiz({this.title, this.candidates, this.answer});
+  Quiz(
+      {required this.title,
+      required this.candidates,
+      required this.answer});
 
   Quiz.fromMap(Map<String, dynamic> map)
-  : title = map['title'],
-  candidates
+      : title = map['title'],
+        candidates = map['candidates'],
+        answer = map['answer'];
 }
--- lib/screen/screen_home.dart
+++ lib/screen/screen_home.dart
@@ -1,4 +1,5 @@
 import 'package:first/model/model_quiz.dart';
+import 'package:first/screen/screen_quiz.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -7,7 +8,7 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  List<Quiz> qizes = [
+  List<Quiz> quizs = [
     Quiz.fromMap({
       'title': 'test',
       'candidates': ['a', 'b', 'c', 'd'],
@@ -42,7 +43,9 @@ class _HomeScreenState extends State<HomeScreen> {
               mainAxisAlignment: MainAxisAlignment.center,
               crossAxisAlignment: CrossAxisAlignment.center,
               children: <Widget>[
-                Center(child: Image.asset('images/무제.png', width: width * 0.4)),
+                Center(
+                    child: Image.asset('images/무제.png',
+                        width: width * 0.4)),
                 Padding(
                   padding: EdgeInsets.all(width * 0.012),
                 ),
@@ -60,28 +63,42 @@ class _HomeScreenState extends State<HomeScreen> {
                 Padding(
                   padding: EdgeInsets.all(width * 0.036),
                 ),
-                _buildStep(width, '1. 랜덤으로 나오는 퀴즈 3개를 풀어보세요.'),
-                _buildStep(width, '2, 문제를 잘 읽고 정답을 고른 뒤\n다음 문제 버튼을 눌러주세요.'),
+                _buildStep(
+                    width, '1. 랜덤으로 나오는 퀴즈 3개를 풀어보세요.'),
+                _buildStep(width,
+                    '2, 문제를 잘 읽고 정답을 고른 뒤\n다음 문제 버튼을 눌러주세요.'),
                 _buildStep(width, '3. 만점을 향해 도전해보세요!'),
                 Padding(
                   padding: EdgeInsets.all(width * 0.048),
                 ),
                 Container(
-                    padding: EdgeInsets.only(bottom: width * 0.024),
+                    padding: EdgeInsets.only(
+                        bottom: width * 0.024),
                     child: Center(
                         child: ButtonTheme(
                             minWidth: width * 0.8,
                             height: height * 0.05,
                             shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(10),
+                              borderRadius:
+                                  BorderRadius.circular(10),
                             ),
                             child: RaisedButton(
                               child: Text(
                                 '지금 퀴즈 풀기',
-                                style: TextStyle(color: Colors.white),
+                                style: TextStyle(
+                                    color: Colors.white),
                               ),
                               color: Colors.deepPurple,
-                              onPressed: () {},
+                              onPressed: () {
+                                Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                    builder: (context) =>
+                                        QuizScreen(
+                                            quizs: quizs),
+                                  ),
+                                );
+                              },
                             ))))
               ],
             )));
@@ -89,14 +106,15 @@ class _HomeScreenState extends State<HomeScreen> {
 
   Widget _buildStep(double width, String title) {
     return Container(
-        padding: EdgeInsets.fromLTRB(
-            width * 0.048, width * 0.024, width * 0.048, width * 0.024),
+        padding: EdgeInsets.fromLTRB(width * 0.048,
+            width * 0.024, width * 0.048, width * 0.024),
         child: Row(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: <Widget>[
             Icon(Icons.check_box, size: width * 0.04),
             Padding(
-              padding: EdgeInsets.only(right: width * 0.024),
+              padding:
+                  EdgeInsets.only(right: width * 0.024),
             ),
             Text(title)
           ],
